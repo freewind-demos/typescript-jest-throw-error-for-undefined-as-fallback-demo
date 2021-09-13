a1db41be1d8aee5a77a64cd060e397e61be7deac
Freewind <nowindlee@gmail.com>
2021-09-13 18:36:35 +0800

    typescript-jest-throw-error-for-undefined-as-fallback-demo

--- hello.test.ts
+++ hello.test.ts
@@ -1,14 +1,32 @@
-function foo() {
-  throw new Error('an error from (foo)');
+export function removeLastStacktrace(err: Error): Error {
+  const stackRows = err.stack?.split('\n');
+  stackRows?.splice(1, 1);
+  err.stack = stackRows?.join('\n');
+  return err;
+}
+
+function fail(message?: string): never {
+  const error = new Error(message)
+  removeLastStacktrace(error);
+  throw error;
+}
+
+function getValue(n: number): number | undefined {
+  if (n < 0) {
+    return undefined
+  } else {
+    return n;
+  }
 }
 
 describe('test', () => {
   it('checks error containing text', () => {
-    // expect(() => foo()).toThrowError(expect.stringContaining('(foo)'))
-    expect(() => foo()).toThrowError('(foo)')
+    const value = getValue(100) ?? fail('value is undefined')
+    expect(value).toBe(100);
   })
 
   it('checks error containing text with regex', () => {
-    expect(() => foo()).toThrowError(/[(]foo[)]/)
+    const value = getValue(-1) ?? fail('value is undefined')
+    expect(value).toBe(undefined);
   })
 })
